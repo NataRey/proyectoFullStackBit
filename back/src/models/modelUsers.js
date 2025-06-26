47532b7f9f89f5b8266fa4eb4dc77e7ff54cd0c9
NataRey <lenarero@gmail.com>
2025-06-25 22:28:24 -0500

    cambios finales clase 25/06/2025

--- back/src/models/modelUsers.js
+++ back/src/models/modelUsers.js
@@ -4,6 +4,7 @@ const schemaUser = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
+    
 });
 
 export default model ('User', schemaUser);
--- back/src/servidor.js
+++ back/src/servidor.js
@@ -1,9 +1,13 @@
 import express from "express";// para realizar la conexion con el servidor 
 import morgan from "morgan";//monitorear solicitudes http
+import cors from "cors";
+import routerUsers from './routers/routerUsers.js';
 
 const servidor = express();//para realizar la conexion con la constante servidor 
+servidor.use(cors());
 servidor.use(morgan("dev"));//para que se actualice conforme vamos realizando los cambios 
 servidor.use(express.json());//para que la conexion que realicemos nos reciba un formato json
+servidor.use('/users', routerUsers);
 
 servidor.get('/',(sol , res)=>{
     res.status(404).send("No encontrado");//por si no encuentra la conexion me enviara esta respuesta 
